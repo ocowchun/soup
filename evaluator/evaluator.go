7037e3ea2da8284b76a12c9748cbf052d169c087
ocowchun <ocowchun@gmail.com>
2025-09-24 09:18:36 +0900

    adjust environment.Update impl: update the value instead of assign a new entry

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -128,45 +128,6 @@ func (n Number) String() string {
 	return fmt.Sprintf("%v", n.data.(float64))
 }
 
-type Environment struct {
-	enclosing *Environment
-	store     map[string]*ReturnValue
-}
-
-func newEnvironment() *Environment {
-	return &Environment{
-		store: make(map[string]*ReturnValue),
-	}
-}
-
-func (env *Environment) Put(key string, value *ReturnValue) {
-	env.store[key] = value
-}
-
-func (env *Environment) Get(key string) (*ReturnValue, bool) {
-	val, ok := env.store[key]
-	if !ok && env.enclosing != nil {
-		return env.enclosing.Get(key)
-	}
-	return val, ok
-}
-
-// Update updates the value of an existing key in the environment and returns the old value.
-// If the key does not exist in the current environment, it recursively
-// checks the enclosing environment. If the key is not found in any
-// environment, it returns an error.
-func (env *Environment) Update(key string, value *ReturnValue) (*ReturnValue, error) {
-	oldVal, ok := env.store[key]
-	if ok {
-		env.store[key] = value
-		return oldVal, nil
-	} else if env.enclosing != nil {
-		return env.enclosing.Update(key, value)
-	}
-
-	return nil, fmt.Errorf("can't find key %s to update", key)
-}
-
 func (e *Evaluator) Eval(program *parser.Program) (*ReturnValue, error) {
 	var ret *ReturnValue
 	var err error
